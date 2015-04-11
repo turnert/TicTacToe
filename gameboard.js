8bdcb67b9383ee3c70fe10fc5b1b3b47df85ef7e
Todd Turner <todd@Todd-Turners-MacBook-Pro.local>
2015-04-11 14:16:39 -0400

    - Added support for constants via a json file - Added support for npm respoitories - Added a game board class that will house all logic relating to the board 	new file:   .gitignore 	new file:   constants.json 	new file:   gameboard.js 	new file:   package.json

---
+++ gameboard.js
@@ -0,0 +1,15 @@
+function GameBoard() {
+    // constructor guard.  Forces new usage, even if invoked
+    if (!(this instanceof GameBoard)) {
+        return new GameBoard();
+    }
+    var board = [new Array(3), new Array(3), new Array(3)];
+
+    function
+
+    function threeInARow() {
+        board[1].
+    }
+}
+
+module.exports = GameBoard;
