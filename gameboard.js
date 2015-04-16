25aa102af4aa0e084792cd81c72e27010f421a11
Todd Turner <todd.turner@utoronto.ca>
2015-04-15 23:14:30 -0400

    - Added support for arbitrarily sized boards - game board can now check if player has filled all the squares in a row 	modified:   gameboard.js

--- gameboard.js
+++ gameboard.js
@@ -1,16 +1,67 @@
 function GameBoard() {
     // constructor guard.  Forces new usage, even if invoked
     if (!(this instanceof GameBoard)) {
-        return new GameBoard();
+        return new GameBoard(config);
     }
-    var board = [new Array(3), new Array(3), new Array(3)],
-        constants = require('konphyg')(__dirname)('constants');
+    var board = [],
+        constants = require('konphyg')(__dirname)('constants'),
+        emptySquares = config.boardSize.numRows * config.boardSize.numColumns;
 
-    function
+    function init() {
+        var rowIndex = 0,
+            columnIndex = 0;
 
-    function threeInARow() {
-        board[constants.middleRowIndex].forEach(function (middleSquare) {
-            
+        for(i = 0;, i < config.boardSize.numColumns) {
+            board.push(new Array[config.boardSize.numRows]);
+        }
+    }
+
+    function isBoardFull() {
+        return emptySquares === 0;
+    }
+
+    function claimSquare(player, position) {
+        if (isValidMove(position)) {
+            board[position.x][position.y] = player.letter;
+            emptySquares = emptySquares - 1;
+        }
+    }
+
+    function contains(position) {
+        return 0 <= position.x && position.x <= config.boardSize.numRows &&
+            0 <= position.y && position.y <= config.boardSize.numColumns;
+    }
+
+    function isValidMove(position) {
+        return contains(position) && board[position.x][position.y] === undefined
+    }
+
+    function winningRow() {
+        var foundWinningRow = false,
+            winningLetter = false;
+        board.forEach(function (row) {
+            var isWinningRow = false;
+            row.forEach(function (columnElement) {
+                winningLetter = winningLetter || columnElement;
+                isWinningRow = winningLetter === columnElement;
+            });
+            foundWinningRow = foundWinningRow || isWinningRow;
+        });
+        return foundWinningRow;
+    }
+
+    function winningColumn() {
+
+    }
+
+    function winningDiagonal() {
+        var index = 0,
+        foundWinningLine = false,
+        winningLetter = false;
+
+        for(index = 0; index < boardSize.numColumns; index += 1) {
+            winningLetter = winningLetter || board[index][index];
+            isWinningLine = winningLetter === board[index][index];
         }
     }
 }
